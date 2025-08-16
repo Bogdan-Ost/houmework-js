7d50d998f0b4b3bb2804f19011df2c0713292374
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2025-08-16 13:02:07 +0300

    adasda

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -110,3 +110,48 @@
 //     console.log(password);
 //   }
 // }
+
+// Використайте цикл while, щоб вивести в console всі числа від 0 до 20 включно.
+
+// let count = 0;
+
+// while (count <= 20) {
+//   console.log(`Count ${count}`);
+//   count += 1;
+// }
+
+// Напишіть функцію getNumbers(min, max), що приймає 2 параметри -
+// мінімальне і максимальне число відповідно.
+// Напишіть цикл всередині функції, який виводить у консоль
+// всі числа від max до min за спаданням.
+// Окрім цього, підрахуйте суму всіх парних чисел в цьому циклі
+// і поверніть її з функції.
+
+// function getNumbers(min, max) {
+//   let sum = 0;
+//   for (let i = max; i >= min; i--) {
+//     console.log(`Number ${i}`);
+//     if (i % 2 === 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+
+// console.log(getNumbers(3, 57));
+
+// Напишіть функцію min(a, b), яка приймає 2 числа і повертає меньше з них.
+// Потрібно додати перевірку, що функція отримує саме числа, в іншому випадку
+// повертати з функції рядок - 'Not a number!'.
+
+// function min(a, b) {
+//   if (!Number.isFinite(a) && !Number.isFinite(b)) {
+//     console.log("Not a number!");
+//   } else if (a > b) {
+//     console.log(b);
+//   } else {
+//     console.log(a);
+//   }
+// }
+
+// console.log(min("23", 43));
